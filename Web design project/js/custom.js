3bfa9d9c35653d6ed8e6065298e4e26ad89622a6
AbdulazizALMousa <127666600+AbdulazizALMousa@users.noreply.github.com>
2023-05-09 16:38:20 +0300

    Add files via upload

---
+++ Web design project/js/custom.js
@@ -0,0 +1,26 @@
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+var modal = document.querySelector(".cart-modal");
+var trigger = document.querySelector(".triggerBtn");
+var closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
